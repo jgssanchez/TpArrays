ef7d3cbc2a15c7cac754821f50381bed56b5aa75
Julio Sanchez <jgs.sanchez94@gmail.com>
2023-11-15 00:58:19 -0300

    ejercicio5 listo

---
+++ js/ejercicio5.js
@@ -0,0 +1,21 @@
+function informacionCadena() {
+  let frase = prompt("Por favor, ingresa una frase:");
+
+  let Mayusculas = frase === frase.toUpperCase();
+  let Minusculas = frase === frase.toLowerCase();
+
+  switch (true) {
+    case Mayusculas:
+      document.write("La frase está formada sólo por mayúsculas.");
+      break;
+    case Minusculas:
+      document.write("La frase está formada sólo por minúsculas.");
+      break;
+    default:
+      document.write(
+        "La frase está formada por mayúsculas y minúsculas."
+      );
+  }
+}
+
+informacionCadena();
